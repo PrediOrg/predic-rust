9ddd16d9b873aacc16317b6420cca20ce53fc295
tarolpotato <71214234+tarolpotato@users.noreply.github.com>
2024-05-10 14:36:23 +0800

    d

--- buy.sh
+++ buy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
-II_ACCOUNT_ID_HEX=$(dfx ledger account-id)
-II_ACCOUNT_ID_VEC=$(python3 -c 'print("vec{" + ";".join([str(b) for b in bytes.fromhex("'$II_ACCOUNT_ID_HEX'")]) + "}")')
-echo $II_ACCOUNT_ID_VEC
+
+#测试web页面的depositeAddress
+ICP_DEPOSIT_ADDRESS=""
+ROOT_PRINCIPAL="principal \"\""
+ICP_DEPOSIT_ADDR_USER1=$(python3 -c 'print("vec{" + ";".join([str(b) for b in bytes.fromhex("'$II_ACCOUNT_ID_HEX'")]) + "}")')
+#测试默认账户的depositeAddress
+ICP_DEPOSIT_ADDR_USER1=$(node scripts/blobToHex.js $(dfx canister call predic getDepositAddress))
+ROOT_PRINCIPAL="principal \"$(dfx identity get-principal)\""
+
 echo "prices:"
 dfx canister call predic getPrices
 echo "depositeAddress:"
-ICP_DEPOSIT_ADDR_USER1=$(node scripts/blobToHex.js $(dfx canister call predic getDepositAddress))
 echo $ICP_DEPOSIT_ADDR_USER1
 echo "transfer to depositeAddress:"
 dfx canister call ledger transfer "(record { amount = record { e8s = 100_001_000 }; to = $ICP_DEPOSIT_ADDR_USER1; fee = record { e8s = 10000}; memo = 1;})"
@@ -15,5 +20,4 @@ dfx canister call ledger account_balance '(record { account = '$ICP_DEPOSIT_ADDR
 echo "buy token:"
 dfx canister call predic buy 0
 echo "ownerNfs:"
-export ROOT_PRINCIPAL="principal \"$(dfx identity get-principal)\""
 dfx canister call predic ownerNfs "($ROOT_PRINCIPAL)"
